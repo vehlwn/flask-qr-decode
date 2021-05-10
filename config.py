93f90339f80f2f1c6d53d716fe2d9cb228ba1fcb
Ivan Bogdanov <c3t3o7@gmail.com>
2021-05-10 09:39:56 +0400

    Disable SQLALCHEMY_TRACK_MODIFICATIONS

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
     def init_app(app):
